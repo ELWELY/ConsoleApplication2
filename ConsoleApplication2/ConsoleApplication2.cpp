4668c2d60fe2b4dd517c8ecb881ad8d4aacf6eec
elwel <elwely64@gmail.com>
2023-10-29 13:08:41 +0100

    #derniére version

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,5 +1,4 @@
 // ConsoleApplication2.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-
 #include <iostream>
 #include <vector>
 #include <cmath>
